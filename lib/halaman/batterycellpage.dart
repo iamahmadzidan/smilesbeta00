abbd5d79b94997e3d48d162eb74070237a6ebe0f
iamahmadzidan <achmadzidann219@gmail.com>
2024-06-21 16:50:45 +0800

    Initial commit

---
+++ lib/halaman/batterycellpage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyBatteryCellPage extends StatelessWidget {
+  const MyBatteryCellPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+   return const Center(
+      child: Text('Informasi Baterai Per Sel'),
+    );
+  }
+}
---
+++ lib/halaman/home.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:smilesbeta00/halaman/batterycellpage.dart';
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key, required String title});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          backgroundColor: const Color(0xFFFFFFFF),
+          appBar: AppBar(
+              title: const Text('Smiles'),
+              titleSpacing: 0,
+              leading: Image.asset('assets/img/smileslogo.png'),
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.question_mark, color: Colors.black),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.settings, color: Colors.black),
+                ),
+              ],
+              toolbarHeight: 70,
+              elevation: 20,
+              flexibleSpace: Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/img/appbarbg.png'),
+                        fit: BoxFit.cover)),
+              ),
+              bottom: const TabBar(
+                tabs: [
+                Tab(
+                  icon: Icon(Icons.electric_bolt_rounded),
+                  text: 'Baterai',
+                ),
+                Tab(
+                  icon: Icon(Icons.rectangle_rounded),
+                  text: 'Sel',
+                )]
+                
+              )),
+          body: const TabBarView(
+            children: [
+              // Halaman 1 (Kesehatan Keseluruhan Baterai)
+              Center(
+                child: Text('Halaman 1: Kesehatan Keseluruhan Baterai'),
+              ),
+              MyBatteryCellPage(), // Panggil widget dari file terpisah
+            ],
+          ),
+        ));
+  }
+}
