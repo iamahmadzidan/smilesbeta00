3b9b633e55c7752ecd70941eeb0cd403d99d14f7
iamahmadzidan <achmadzidann219@gmail.com>
2024-07-08 10:24:02 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
         ),
-      home: const MyHome(title: 'Home Page'),
+      home: const MyHome(),
     );
   }
 }
