10bd1a408fb33a872f639624b9439cd5448fe8fb
iamahmadzidan <achmadzidann219@gmail.com>
2024-07-12 22:41:16 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:smilesbeta00/halaman/home.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:smilesbeta00/halaman/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,8 +19,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
-        ),
-      home: const MyHome(),
+      ),
+      home: const MyHome()
     );
   }
 }
